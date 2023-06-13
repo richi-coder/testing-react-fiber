6c096d4c26bb04eafe72533a79304478006e57c5
richi-coder <richicoder@gmail.com>
2023-06-13 18:57:40 -0400

    using useFrame instead of useState, requires frameloop to be set at always

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Canvas } from '@react-three/fiber'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Canvas camera={{ position: [-400,15,-40] }} frameloop="always">
+        <App />
+        <ambientLight intensity={0.5}/>
+        <directionalLight color='red' position={[0,0,5]} />
+    </Canvas>
 )
