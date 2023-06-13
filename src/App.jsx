27736319a7b1fee69031fc517983bc981575165a
richi-coder <richicoder@gmail.com>
2023-06-12 22:31:59 -0400

    positioning the delorian

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 function App() {
   const gltf = useLoader(GLTFLoader, 'scene.gltf')
   return (
-    <Canvas camera={{ position: [500,500,500]}}>
+    <Canvas camera={{ position: [-200,100,-500],}} frameloop="demand">
       <ambientLight intensity={0.1}/>
       <directionalLight color='red' position={[0,0,5]} />
-      <primitive object={gltf.scene} />
+      <primitive object={gltf.scene} position={[0,0,0]}/>
     </Canvas>
     
   )
